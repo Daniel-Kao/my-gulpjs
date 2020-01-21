c9c643df2684b127fa588b2c51f6efae847f0e51
daniel <daniel@192.168.124.14>
2020-01-21 08:23:33 +0800

    preserve structure
    Signed-off-by: daniel <daniel@192.168.124.14>

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,79 @@
 const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
-var uglify = require("gulp-uglify");
-var pipeline = require("readable-stream").pipeline;
+// const imagemin = require("gulp-imagemin");
+// var uglify = require("gulp-uglify");
+// var pipeline = require("readable-stream").pipeline;
 var sass = require("gulp-sass");
-var babel = require("gulp-babel");
-var connect = require("gulp-connect");
-var proxy = require("http-proxy-middleware");
-const config = require("config");
-const API = config.get("API");
-const PROXY = config.get("PROXY");
+// var babel = require("gulp-babel");
+// var connect = require("gulp-connect");
+// var proxy = require("http-proxy-middleware");
+// const config = require("config");
+// const API = config.get("API");
+// const PROXY = config.get("PROXY");
 
 sass.compiler = require("node-sass");
 
-function sassBuild() {
+gulp.task("sassBuild", function() {
   return gulp
-    .src("./src/sass/*.scss")
+    .src(["src/*/assets/sass/*.scss"], { base: "./src/" })
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("dist/sass"));
-}
+    .pipe(gulp.dest("public/"));
+});
 
-function message(cb) {
-  console.log("gulp running");
-  cb();
-}
+// function sassBuild() {
+//   return gulp
+//     .src(["./src/*/sass/*.scss"], { base: "./src/" })
+//     .pipe(sass().on("error", sass.logError))
+//     .pipe(gulp.dest("./public/assets/"));
+// }
 
-function copyHtml() {
-  return gulp.src("src/h5/*.html").pipe(gulp.dest("dist/h5"));
-}
+// function message(cb) {
+//   console.log("gulp running");
+//   cb();
+// }
 
-function imageMin(cb) {
-  gulp
-    .src("src/images/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("dist/images"));
-  cb();
-}
+// function copyHtml() {
+//   return gulp.src("src/h5/*.html").pipe(gulp.dest("dist/h5"));
+// }
 
-function compress() {
-  return pipeline(
-    gulp.src("src/js/*.js"),
-    babel({
-      presets: ["@babel/preset-env"]
-    }),
-    uglify(),
-    gulp.dest("dist/js")
-  );
-}
+// function imageMin(cb) {
+//   gulp
+//     .src("src/images/*")
+//     .pipe(imagemin())
+//     .pipe(gulp.dest("dist/images"));
+//   cb();
+// }
 
-function dev() {
-  connect.server({
-    root: ["./dist"],
-    middleware: function(connect, opt) {
-      return [
-        proxy(`/${API}`, {
-          target: `${PROXY}`,
-          changeOrigin: true
-        })
-      ];
-    }
-  });
-}
+// function compress() {
+//   return pipeline(
+//     gulp.src("src/js/*.js"),
+//     babel({
+//       presets: ["@babel/preset-env"]
+//     }),
+//     uglify(),
+//     gulp.dest("dist/js")
+//   );
+// }
 
-function watch() {
-  gulp.watch(["./src/h5/*.html"], function() {
-    return gulp.src("src/h5/*.html").pipe(gulp.dest("dist/h5"));
-  });
-}
+// function dev() {
+//   connect.server({
+//     root: ["./dist"],
+//     middleware: function(connect, opt) {
+//       return [
+//         proxy(`/${API}`, {
+//           target: `${PROXY}`,
+//           changeOrigin: true
+//         })
+//       ];
+//     }
+//   });
+// }
 
-exports.build = gulp.parallel(message, copyHtml, compress, sassBuild);
+// function watch() {
+//   gulp.watch(["./src/h5/*.html"], function() {
+//     return gulp.src("src/h5/*.html").pipe(gulp.dest("dist/h5"));
+//   });
+// }
 
-exports.dev = gulp.parallel(message, watch, dev);
+// exports.build = gulp.parallel(message, copyHtml, compress, sassBuild);
+
+// exports.dev = gulp.parallel(message, watch, dev);
